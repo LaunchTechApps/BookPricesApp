905bed71693c6a414294268ec285bddc51cc95db
Paul Peters <paul.peters144@gmail.com>
2024-03-26 09:07:00 -0500

    added amazon and ebay tabs

--- BookPricesApp/Form1.Designer.cs
+++ BookPricesApp/Form1.Designer.cs
@@ -29,10 +29,15 @@ partial class BookPrices
     private void InitializeComponent()
     {
         version_lbl = new Label();
-        panel1 = new Panel();
-        label1 = new Label();
-        label3 = new Label();
-        panel1.SuspendLayout();
+        amazon_pnl = new Panel();
+        amazon_lbl = new Label();
+        amazon_tb = new Label();
+        ebay_tb = new Label();
+        ebay_lbl = new Label();
+        ebay_pnl = new Panel();
+        label2 = new Label();
+        amazon_pnl.SuspendLayout();
+        ebay_pnl.SuspendLayout();
         SuspendLayout();
         // 
         // version_lbl
@@ -44,56 +49,109 @@ partial class BookPrices
         version_lbl.TabIndex = 5;
         version_lbl.Text = "...";
         // 
-        // panel1
+        // amazon_pnl
         // 
-        panel1.BorderStyle = BorderStyle.FixedSingle;
-        panel1.Controls.Add(label1);
-        panel1.Location = new Point(42, 50);
-        panel1.Name = "panel1";
-        panel1.Size = new Size(492, 519);
-        panel1.TabIndex = 6;
+        amazon_pnl.BorderStyle = BorderStyle.FixedSingle;
+        amazon_pnl.Controls.Add(amazon_lbl);
+        amazon_pnl.Location = new Point(42, 50);
+        amazon_pnl.Name = "amazon_pnl";
+        amazon_pnl.Size = new Size(492, 519);
+        amazon_pnl.TabIndex = 6;
         // 
-        // label1
+        // amazon_lbl
         // 
-        label1.AutoSize = true;
-        label1.Location = new Point(237, 251);
-        label1.Name = "label1";
-        label1.Size = new Size(57, 15);
-        label1.TabIndex = 6;
-        label1.Text = "LabelOne";
+        amazon_lbl.AutoSize = true;
+        amazon_lbl.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+        amazon_lbl.Location = new Point(181, 17);
+        amazon_lbl.Name = "amazon_lbl";
+        amazon_lbl.Size = new Size(85, 25);
+        amazon_lbl.TabIndex = 0;
+        amazon_lbl.Text = "Amazon";
         // 
-        // label3
+        // amazon_tb
         // 
-        label3.AutoSize = true;
-        label3.BorderStyle = BorderStyle.FixedSingle;
-        label3.Location = new Point(42, 30);
-        label3.Name = "label3";
-        label3.Size = new Size(70, 17);
-        label3.TabIndex = 8;
-        label3.Text = "   Tab One   ";
+        amazon_tb.AutoSize = true;
+        amazon_tb.BorderStyle = BorderStyle.FixedSingle;
+        amazon_tb.Location = new Point(42, 24);
+        amazon_tb.Name = "amazon_tb";
+        amazon_tb.Padding = new Padding(3);
+        amazon_tb.Size = new Size(77, 23);
+        amazon_tb.TabIndex = 8;
+        amazon_tb.Text = "   Amazon   ";
+        amazon_tb.Click += amazon_tb_Click;
+        // 
+        // ebay_tb
+        // 
+        ebay_tb.AutoSize = true;
+        ebay_tb.BorderStyle = BorderStyle.FixedSingle;
+        ebay_tb.Location = new Point(125, 24);
+        ebay_tb.Name = "ebay_tb";
+        ebay_tb.Padding = new Padding(3);
+        ebay_tb.Size = new Size(70, 23);
+        ebay_tb.TabIndex = 9;
+        ebay_tb.Text = "     Ebay     ";
+        ebay_tb.Click += ebay_tb_Click;
+        // 
+        // ebay_lbl
+        // 
+        ebay_lbl.AutoSize = true;
+        ebay_lbl.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+        ebay_lbl.Location = new Point(212, 17);
+        ebay_lbl.Name = "ebay_lbl";
+        ebay_lbl.Size = new Size(54, 25);
+        ebay_lbl.TabIndex = 0;
+        ebay_lbl.Text = "Ebay";
+        // 
+        // ebay_pnl
+        // 
+        ebay_pnl.BorderStyle = BorderStyle.FixedSingle;
+        ebay_pnl.Controls.Add(ebay_lbl);
+        ebay_pnl.Location = new Point(42, 50);
+        ebay_pnl.Name = "ebay_pnl";
+        ebay_pnl.Size = new Size(492, 519);
+        ebay_pnl.TabIndex = 11;
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(522, 572);
+        label2.Name = "label2";
+        label2.Size = new Size(16, 15);
+        label2.TabIndex = 10;
+        label2.Text = "...";
         // 
         // BookPrices
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(584, 636);
-        Controls.Add(label3);
-        Controls.Add(panel1);
+        Controls.Add(ebay_pnl);
+        Controls.Add(label2);
+        Controls.Add(ebay_tb);
+        Controls.Add(amazon_tb);
+        Controls.Add(amazon_pnl);
         Controls.Add(version_lbl);
         MaximizeBox = false;
         MaximumSize = new Size(600, 675);
         MinimumSize = new Size(600, 675);
         Name = "BookPrices";
         Text = "Book Prices";
-        panel1.ResumeLayout(false);
-        panel1.PerformLayout();
+        amazon_pnl.ResumeLayout(false);
+        amazon_pnl.PerformLayout();
+        ebay_pnl.ResumeLayout(false);
+        ebay_pnl.PerformLayout();
         ResumeLayout(false);
         PerformLayout();
     }
 
     #endregion
     private Label version_lbl;
-    private Panel panel1;
-    private Label label1;
+    private Label amazon_tb;
+    private Panel amazon_pnl;
     private Label label3;
+    private Label ebay_tb;
+    private Label amazon_lbl;
+    private Label ebay_lbl;
+    private Panel ebay_pnl;
+    private Label label2;
 }
--- BookPricesApp/Form1.cs
+++ BookPricesApp/Form1.cs
@@ -2,17 +2,34 @@ namespace BookPrices;
 
 public partial class BookPrices : Form
 {
-    List<Panel> _panelList = new List<Panel>();
+    private List<Panel> _panelList = new();
+    private List<Label> _tabList = new();
+
     int index = 0;
     public BookPrices()
     {
         InitializeComponent();
-        _panelList.AddRange(new List<Panel>() { panel1 });
+        _panelList.AddRange(new List<Panel>() { amazon_pnl, ebay_pnl });
+        _tabList.AddRange(new List<Label>() { amazon_tb, ebay_tb });
         _panelList[0].BringToFront();
     }
 
     private void BookPrices_Load(object sender, EventArgs e)
     {
 
+    }
+
+    private void amazon_tb_Click(object sender, EventArgs e)
+    {
+        _tabList.ForEach(t => t.BackColor = Color.FromName("Control"));
+        _tabList[0].BackColor = Color.FromName("ActiveBorder");
+        _panelList[0].BringToFront();
+    }
+
+    private void ebay_tb_Click(object sender, EventArgs e)
+    {
+        _tabList.ForEach(t => t.BackColor = Color.FromName("Control"));
+        _tabList[1].BackColor = Color.FromName("ActiveBorder");
+        _panelList[1].BringToFront();
     }
 }
